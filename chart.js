a4b8f46e4135dccca4f7fc73bb4d6d59b0abb963
Katarzyna Pi <38461666+katarzynapi@users.noreply.github.com>
2018-06-08 23:34:20 +0200

    Wyniki
    zabarwienie słupka, który odpowiada danemu uczestnikowi ankiety

--- chart.js
+++ chart.js
@@ -10,11 +10,13 @@ function drawChart() {
 	var options = {
 		legend: {position:'none'},
 		title: "Twój poziom neurotyczności na tle innych ankietowanych",
-		titleTextStyle: {fontSize: 25},
-		bar: {groupWidth: "80%"},
-		hAxis: {title: 'Stopień neurotyczności [%]', titleTextStyle: {fontSize: 15}},
+		titleTextStyle: {fontSize: 20},
+		bar: {groupWidth: "100%"},
+		hAxis: {title: 'Poziom neurotyczności [%]', titleTextStyle: {fontSize: 15}},
 		vAxis: {title: 'Liczba osób', titleTextStyle: {fontSize: 15}},
-		colors: ['#6CDAB2'],
+		colors: ['#a7e3d1'],
+		isStacked: true,
+  
 	};
 	var data = new google.visualization.DataTable(jsonData);
 	var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
